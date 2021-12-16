623f6e951d0731c01de5f4297f11b3d82fe6c53c
earningsites-net <96264757+earningsites-net@users.noreply.github.com>
2021-12-16 22:06:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.querySelector("#root")
+);
